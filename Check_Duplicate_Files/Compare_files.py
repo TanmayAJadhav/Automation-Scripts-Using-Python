29673e2adceb7b9c13564e2085134f1c988ea24e
Tanmay Jadhav <87440930+TanmayAJadhav@users.noreply.github.com>
2022-03-11 18:27:18 +0530

    Update Compare_files.py

--- Check_Duplicate_Files/Compare_files.py
+++ Check_Duplicate_Files/Compare_files.py
@@ -1,4 +1,4 @@
-# 4)Write a program which accept two file names from user and compare contents of both the files. If both the files contains same contents then display 
+# Q)Write a program which accept two file names from user and compare contents of both the files. If both the files contains same contents then display 
 # success otherwise display failure. Accept names of both the files from command line. 
 # Input : Demo.txt Hello.txt 
 # Compare contents of Demo.txt and Hello.txt 
